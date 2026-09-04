a5358c7a4c48f4b1dfdd6bb4c5ab341d9b8ebdf7
Mohammad Ashraful Alam <mohammadashrafulalam11@gmail.com>
2026-09-04 18:20:18 +0600

    Enhance product card with sale price and discount

--- index.php
+++ index.php
@@ -21,12 +21,18 @@ require 'config.php';
         $result = $conn->query("SELECT * FROM products");
         while ($row = $result->fetch_assoc()) {
             $img = !empty($row['image']) ? $row['image'] : 'https://placehold.co/400x500?text=No+Image';
+            $sale_price = $row['price'] - ($row['price'] * $row['discount_percent'] / 100);
             
             echo "<div class='product-card'>";
-            echo "<a href='product.php?id={$row['id']}'>";
+            echo "<a href='product.php?id={$row['id']}' style='text-decoration:none; display:block;'>";
             echo "<img src='{$img}' alt='{$row['name']}'>";
             echo "<div class='product-title'>{$row['name']}</div>";
-            echo "<div class='product-price'>{$row['price']} TK</div>";
+            
+            if ($row['discount_percent'] > 0) {
+                echo "<div class='product-price'><span class='original-price'>{$row['price']} TK</span> <span class='sale-price'>{$sale_price} TK</span><span class='discount-badge'>-{$row['discount_percent']}%</span></div>";
+            } else {
+                echo "<div class='product-price' style='font-weight:bold; color:#000;'>{$row['price']} TK</div>";
+            }
             echo "</a>";
             echo "</div>";
         }
